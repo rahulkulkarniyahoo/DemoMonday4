9b56442af96eab74033bb49b7a10e05facd13bc2
rahul.kulkarni <rahul.kulkarni@mailnewspapers.co.uk>
2015-10-26 14:17:47 +0000

    Squashed commit of the following:
    commit 5cc3ff46fba7b17016d8f5d315b5303a60d5b4c5
    Author: rahul.kulkarni <rahul.kulkarni@mailnewspapers.co.uk>
    Date:   Mon Oct 26 14:17:34 2015 +0000
    
    	Finished with Windows Feature1

--- DemoMonday4/src/demopkg4/DemoClass4.java
+++ DemoMonday4/src/demopkg4/DemoClass4.java
@@ -5,5 +5,6 @@ public class DemoClass4 {
 	public static void main(String[] args) {
 		System.out.println("This is first view");
 		System.out.println("This is Mac Dev 01");
+		System.out.println("This is Windows Feature 01");
 	}
 }
